5acc7fc0587ea78f91fdcf4ccfbe12cf0a8f8e26
Jen Ziel <jenziel400@gmail.com>
2023-08-04 18:14:39 -0400

    Add images to assets folder.  Add sign-in page layout.

--- src/scripts.js
+++ src/scripts.js
@@ -6,6 +6,11 @@ import './css/styles.css';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
-
+import './images/logo3.png'
+import './images/globe_logo.jpg'
+import './images/logo9.png'
+import './images/profile-logo.png'
+import './images/bell1.png'
+import './images/globe-icon.png'
 
 console.log('This is the JavaScript entry file - your code begins here.');

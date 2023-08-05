b1a0c21312d8a2ed678fa8f51c2ad08186e98a84
Jen Ziel <jenziel400@gmail.com>
2023-08-05 18:18:44 -0400

    Add basic site layout'

--- src/scripts.js
+++ src/scripts.js
@@ -12,5 +12,66 @@ import './images/logo9.png'
 import './images/profile-logo.png'
 import './images/bell1.png'
 import './images/globe-icon.png'
+import './images/italy.png'
+
+// Import domUpdates
+
+import 
+{signInButton,
+showMainPage,
+updateTripsPage,
+getDestinationNums,
+getDestinationNames, 
+} from './domUpdates';
+
+//Import functions
+import { getUserTrips, getUpcomingTrips, createTripCard  } from './functions';
+
+// Import API Calls
+import {getData} from './apiCalls';
+
+// Main Data Object
+
+const mainData = {
+    today: '2023/08/04',
+}
+
+signInButton.addEventListener('click', showMainPage)
+
+window.addEventListener('load', () => {
+    getData
+    Promise.all(getData()).then((promises) => {
+        mainData.trips = promises[0].trips;
+        mainData.travelers = promises[1].travelers;
+        mainData.destinations = promises[2].destinations;
+        console.log('mainData', mainData);
+        console.log('mainData.trips', mainData.trips)
+        console.log('mainData.travelers', mainData.travelers)
+        getUserData()
+    })
+    .then(getDescriptiveData())
+    .then(generatePage())
+  })
+
+
+
+  const getUserData = () => {
+    mainData.currentUser = mainData.travelers[2].id
+    mainData.userTrips = getUserTrips(mainData.trips, mainData.currentUser)
+    console.log('mainData.currentUser',  mainData.currentUser)
+    console.log('mainData.userTrips', mainData.userTrips)
+  }
+  const getDescriptiveData = () => {
+    mainData.userNums = getDestinationNums(mainData.userTrips)
+    mainData.locationNames = getDestinationNames(mainData.userNums, mainData.destinations)
+    console.log("mainData.userNums", mainData.userNums)
+    console.log("mainData.locationNames", mainData.locationNames)
+    mainData.tripCards = createTripCard(mainData.userTrips, mainData.destinations)
+  }
+  const generatePage = () => {
+    mainData.currentUser = mainData.travelers[0];
+    updateTripsPage(mainData.travelers[0].name, mainData.locationNames, mainData.trips[0].destinationID)
+  }
+
 
 console.log('This is the JavaScript entry file - your code begins here.');

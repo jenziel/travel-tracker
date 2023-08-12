aef4ddc2a72d75e6bb2c99826d6009c28e2bcc60
Jen Ziel <jenziel400@gmail.com>
2023-08-12 13:16:41 -0400

    Fix syntax for endpoints

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,9 +1,9 @@
 console.log("fetch requests go here");
-const tripsUrl = "http://https://ziel-travel-tracker-cdnarcynv-jenziel.vercel.app//api/v1/trips";
-const travelersUrl = "http://https://ziel-travel-tracker-cdnarcynv-jenziel.vercel.app//api/v1/travelers";
-const specificTravelersUrl = `http://https://ziel-travel-tracker-cdnarcynv-jenziel.vercel.app//api/v1/travelers/50`;
-const destinationsUrl = "http://https://ziel-travel-tracker-cdnarcynv-jenziel.vercel.app//api/v1/destinations";
-const newTripUrl = "http://https://ziel-travel-tracker-cdnarcynv-jenziel.vercel.app//api/v1/trips";
+const tripsUrl = "http://https://ziel-travel-tracker-cdnarcynv-jenziel.vercel.app/api/v1/trips";
+const travelersUrl = "http://https://ziel-travel-tracker-cdnarcynv-jenziel.vercel.app/api/v1/travelers";
+const specificTravelersUrl = `http://https://ziel-travel-tracker-cdnarcynv-jenziel.vercel.app/api/v1/travelers/50`;
+const destinationsUrl = "http://https://ziel-travel-tracker-cdnarcynv-jenziel.vercel.app/api/v1/destinations";
+const newTripUrl = "http://https://ziel-travel-tracker-cdnarcynv-jenziel.vercel.app/api/v1/trips";
 const endpoints = [tripsUrl, travelersUrl, destinationsUrl];
 import dayjs from "dayjs";
 
